4e0bc85e886879d88417ad59977522bcd9fc1cc0
Sagar Makwana <sagarmak4@gmail.com>
2023-04-23 15:39:10 +0530

    linear-search

---
+++ linear-search.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* @author     Sagar Makwana
+* @datetime   23 March 2023
+* @perpose    Standard commenting way
+* @input      arr[] = {10, 50, 30, 70, 80, 20, 90, 40} and key = 20
+* @output     Eelment present in 5
+*/
+?>
