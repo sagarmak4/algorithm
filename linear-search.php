88fbd5c4fd09a35dd60660eb1f2b9a7daa429ca3
Sagar Makwana <sagarmak4@gmail.com>
2023-04-23 17:33:10 +0530

    Done indentation

--- linear-search.php
+++ linear-search.php
@@ -1,21 +1,23 @@
 <?php
 /**
-* @author     Sagar Makwana
-* @datetime   23 March 2023
-* @perpose    Standard commenting way
-* @input      arr[] = {2,3,4,5,10,40} and key = 10
-* @output     Eelment present in 4
-*/
+ * @author     Sagar Makwana
+ * @datetime   23 March 2023
+ * @perpose    Standard commenting way
+ * @input      arr[] = {2,3,4,5,10,40} and key = 10
+ * @output     Eelment present in 4
+ */
 
-$arr = array(2,3,4,5,10,40);
+$arr = [2, 3, 4, 5, 10, 40];
 $x = 10;
 
-function search ($arr,$x){
+function search($arr, $x)
+{
     $n = sizeof($arr);
-    for($i=0;$i<$n;$i++){
-        if($arr[$i] == $x){
+    for ($i = 0; $i < $n; $i++) {
+        if ($arr[$i] == $x) {
             return $i;
-        }return -1;
+        }
+        return -1;
     }
 }
 
